953c5c45b401c9e6547e78cb1a2a56e8ce1c97fb
mahmoud88 <mah.mphammed88@gmail.com>
2020-11-23 13:17:29 -0800

    derf update

--- src/course/course.service.ts
+++ src/course/course.service.ts
@@ -5,22 +5,22 @@ import { UpdateCourseDto } from './dto/update-course.dto';
 @Injectable()
 export class CourseService {
   create(createCourseDto: CreateCourseDto) {
-    return 'This action adds a new course';
+    return 'This action adds a new course3333';
   }
 
   findAll() {
-    return `This action returns all course`;
+    return `This action returns all course333`;
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} course`;
+    return `This action returns a #${id} course333`;
   }
 
   update(id: number, updateCourseDto: UpdateCourseDto) {
-    return `This action updates a #${id} course`;
+    return `This action updates a #${id} course333`;
   }
 
   remove(id: number) {
-    return `This action removes a #${id} course`;
+    return `This action removes a #${id} course333`;
   }
 }
