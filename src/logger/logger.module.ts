536bece2b6ec763286853cad479b9e5ee2da0f6f
mahmoud88 <mah.mphammed88@gmail.com>
2020-11-27 07:30:07 -0800

    clean solution

--- src/logger/logger.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { LoggerService } from './logger.service';
-import { LoggerController } from './logger.controller';
-
-@Module({
-  controllers: [LoggerController],
-  providers: [LoggerService]
-})
-export class LoggerModule {}
