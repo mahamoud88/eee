2bbf67e9d6a681c6541edba5fae5b11deb4e1a3f
mahmoud88 <mah.mphammed88@gmail.com>
2020-11-27 14:16:48 -0800

    exceptionfilter

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,17 +6,27 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { typeOrmConfig } from './config/typeorm.config';
-import { CourseModule } from './course/course.module';
-import { LoggerModule } from './logger/logger.module';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpErrorFilter } from './shard/http-error.filter';
+ 
  
  
  
  
 
 @Module({
- imports:[ TypeOrmModule.forRoot(typeOrmConfig), AuthModule, CourseModule, LoggerModule,],
+ imports:[ TypeOrmModule.forRoot(typeOrmConfig), AuthModule,  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpErrorFilter,
+    }
+  
+  
+  
+  ],
   
 })
 export class AppModule {}
